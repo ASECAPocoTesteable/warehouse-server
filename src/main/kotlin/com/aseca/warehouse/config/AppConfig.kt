0bed1190ec6de15882e91cd3c5877d4514208012
BraRIOS <braian.rios@ing.austral.edu.ar>
2024-06-03 22:37:45 -0300

    add product stock integration with front

--- src/main/kotlin/com/aseca/warehouse/config/AppConfig.kt
+++ src/main/kotlin/com/aseca/warehouse/config/AppConfig.kt
@@ -3,11 +3,26 @@ package com.aseca.warehouse.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class AppConfig {
     @Bean
     fun webClient(): WebClient {
         return WebClient.builder().build()
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3004")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
     }
 }
